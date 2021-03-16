801ddc2418c267e7f892e3448c692d62da10486f
oxirusss <63094117+oxirusss@users.noreply.github.com>
2021-03-16 01:46:14 -0500

    Завершаем вёрстку внутренней страницы

--- js/modal-feedback.js
+++
@@ -1,6 +0,0 @@
-const feedbackButton = document.querySelector(".contacts__button");
-
-feedbackButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    console.log("Клик по ссылке");
-});
